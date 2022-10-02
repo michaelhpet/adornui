8e04ed47e48c5426f3c482c3cf3eef0166f52ad8
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-02 13:07:21 +0100

    firebase hosting dir as dist/

---
+++ apps/docs/dist/_next/static/CiI1wVgcSIcoxVPWq6VGa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9f8e5a29ad5b95ba.js"],"/_error":["static/chunks/pages/_error-ec76108ee1ddf888.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
