d53523db779e8669097a936b77c03441468161da
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-02 12:48:41 +0100

    initial commit

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Button } from "ui";
+import { Button } from 'ui';
 
 export default function Docs() {
   return (
     <div>
-      <h1>Docs</h1>
+      <h1>Adorn UI Docs</h1>
       <Button />
     </div>
   );
