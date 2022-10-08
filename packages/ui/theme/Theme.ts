41eed59e4f4dd22c81f0e8b10484a516fa37c6a5
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-08 21:18:43 +0100

    nothing fancy

--- packages/ui/theme/Theme.ts
+++ packages/ui/theme/Theme.ts
@@ -5,5 +5,10 @@ export default interface Theme {
   palette: Palette;
   typography: {
     h1: Typography;
+    h2: Typography;
+    h3: Typography;
+    h4: Typography;
+    h5: Typography;
+    h6: Typography;
   };
 }
